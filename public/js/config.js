f88b5a71b9b4e3e1a77bfa64c263faa42709dc41
Enrique Aguilar <enriqueao96@gmail.com>
2018-06-22 18:28:11 -0500

    correciones menores

--- public/js/config.js
+++ public/js/config.js
@@ -1,7 +1,7 @@
 var config = {
-                url: "http://148.220.209.207/txtr/",
-                img: "http://148.220.209.207/txtr/public/img/"
+                url: "http://localhost/txtr/",
+                img: "http://localhost/txtr/public/img/"
             }
             var imported = document.createElement("script");
-            imported.src = "http://148.220.209.207/txtr/public/js/complementos.js";
+            imported.src = "http://localhost/txtr/public/js/complementos.js";
             document.head.appendChild(imported);
